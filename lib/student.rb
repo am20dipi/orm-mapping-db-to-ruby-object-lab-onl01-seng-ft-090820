147ed8188da12566ffc72db1e827f364e1d0cde1
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 22:48:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -92,13 +92,12 @@ class Student
       SELECT * 
       FROM students 
       WHERE grade = 10
-      ORDER BY id
       LIMIT 1
     SQL
     
     DB[:conn].execute(sql, x).map do |row|
-      self.all
-    end
+      self.new_from_db(row)
+    end.first
   end
   
   def self.first_student_in_grade_10
