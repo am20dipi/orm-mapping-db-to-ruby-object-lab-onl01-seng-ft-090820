0d1faa408b0b88e692b75719fde3258ea2fbce1e
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 22:54:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,14 +87,14 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10(name)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT *
       FROM students 
       WHERE grade = 10
     SQL
     
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end[0][1]
 
