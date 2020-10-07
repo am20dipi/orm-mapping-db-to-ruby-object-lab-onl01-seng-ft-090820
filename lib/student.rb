e5833d5a6e116fbe5281e3d83670017ec47b4cf2
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 22:46:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,7 +87,7 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10(count)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT * 
       FROM students 
