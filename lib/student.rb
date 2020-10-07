be04e92b7cc842bd108f25ea31c46f0322d9e514
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 22:11:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,7 +77,14 @@ class Student
   end
   
   def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+    SQL
     
+    DB[:conn].execute(sql).map do |row|
+      self.all
   end
   
   def self.first_X_students_in_grade_10()
