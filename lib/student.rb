92e13d069c058329c9493cf70d6e17963539c4dc
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 22:58:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -92,12 +92,12 @@ class Student
       SELECT *
       FROM students 
       WHERE grade = 10
+      LIMIT = ?
     SQL
     
-    DB[:conn].execute(sql, x).map do |name|
-      self.new_from_db(name)
-    end[0][1]
-
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
   end
   
   def self.first_student_in_grade_10
