b5bf4044ce2371506f58ec8728e76e6115f30dc6
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 22:47:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,7 +87,7 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10(X)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT * 
       FROM students 
@@ -96,7 +96,7 @@ class Student
       LIMIT 1
     SQL
     
-    DB[:conn].execute(sql, X).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.all
     end
   end
