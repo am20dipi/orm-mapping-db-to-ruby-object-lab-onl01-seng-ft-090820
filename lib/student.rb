8a5ed686383c9e246c3589e4d68e65455b6a540d
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 22:48:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -89,7 +89,7 @@ class Student
   
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
-      SELECT * 
+      SELECT SUM(students) 
       FROM students 
       WHERE grade = 10
       LIMIT 1
