8dd7b7ab5c22564a321da90feb48a5bfee6f2ee3
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 21:57:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,12 @@
 class Student
   attr_accessor :id, :name, :grade
 
-  def self.new_from_db(row)
-    # create a new Student object given a row from the database
+  def self.new_from_db(row)# create a new Student object given a row from the database, converting the db into a RUBY obj
+    new_student = self.new 
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
   end
 
   def self.all
