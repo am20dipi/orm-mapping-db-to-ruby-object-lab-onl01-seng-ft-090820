3e687f0fd064d338fceb7153a8d60a049d3827de
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 22:16:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,7 +77,7 @@ class Student
   
   def self.students_below_12th_grade
     sql = <<-SQL
-      SELECT name.students
+      SELECT names.students
       FROM students
       WHERE grade <= 11
     SQL
