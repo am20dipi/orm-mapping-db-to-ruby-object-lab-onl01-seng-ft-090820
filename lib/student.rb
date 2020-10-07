5ed9bad8ba866e7b44661532afe9818b918c1221
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 22:15:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -77,7 +77,7 @@ class Student
   
   def self.students_below_12th_grade
     sql = <<-SQL
-      SELECT *
+      SELECT students
       FROM students
       WHERE grade <= 11
     SQL
@@ -85,10 +85,11 @@ class Student
     DB[:conn].execute(sql).map do |row|
       self.all
     end
-    students
+    
   end
   
-  def self.first_X_students_in_grade_10()
+  def self.first_X_students_in_grade_10(X)
+    
     
   end
   
