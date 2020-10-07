71df5b199a49283ab127ef8bfdecd4a261900eb9
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-07 22:06:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,4 +65,21 @@ class Student
   
   def self.all_students_in_grade_9
     
+  end
+  
+  def self.students_below_12th_grade
+    
+  end
+  
+  def self.first_X_students_in_grade_10()
+    
+  end
+  
+  def self.first_student_in_grade_10
+    
+  end
+  
+  def self.all_students_in_grade_x
+    
+  end
 end
